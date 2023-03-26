b09a17c37ad0832659aa6397c5737580d6e17340
ConstantBounoure <constant.bounoure@gmail.com>
2023-03-26 21:22:43 +0200

    reducer first commit

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState, useEffect, useReducer } from "react";
 import { onAuthStateChangedListener } from "../utils/firebase/firebase.utils";
 import { createUserDocumentFromAuth } from "../utils/firebase/firebase.utils";
 
@@ -6,6 +6,20 @@ export const UserContext = createContext({
     currentUser: null,
     setCurrentUser: () => null,
 });
+
+// const userReducer = (state, action) => {
+//     const { type, payload } = action;
+
+//     switch (type) {
+//         case "SET_CURRENT_USER":
+//             return { ...state, currentUser: payload };
+
+//         default:
+//             throw new Error(`Unhandled type ${type} in userReducer`);
+//     }
+
+//     return {};
+// };
 
 export const UserProvider = ({ children }) => {
     const [currentUser, setCurrentUser] = useState(null);
